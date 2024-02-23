21f3a1f68fb48eab89f3442580625cf0ba3e9254
srivaishnavi2 <160583615+srivaishnavi2@users.noreply.github.com>
2024-02-23 08:15:24 +0530

    Add files via upload

---
+++ day 4/pro10.py
@@ -0,0 +1,14 @@
+#write a python program to print word,count of the vowels in the word,list of the vowels in the word of the sentence using functions.
+def counting(s):
+    print(s)
+    s1,vc='',0
+    for i in s:
+        if i.lower() in "aeiou":
+            vc+=1
+            s1+=i
+    print("vowel count:",vc)
+    print(list(set(s1)))
+l=input().split()
+for i in l:
+    counting(i)
+        
---
+++ day 4/pro5.py
@@ -0,0 +1,9 @@
+#to print the average of the matrix
+r,c=int(input()),int(input())
+l,sum=[],0
+for i in range(r):
+    l.append(tuple(map(int,input().split())))
+for i in l:
+    for j in i:
+       sum+=j
+print(sum/(r*c))
