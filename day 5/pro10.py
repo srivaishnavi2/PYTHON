cdebbf89be337805fbd6c88009ed4e45409cbb6a
srivaishnavi2 <160583615+srivaishnavi2@users.noreply.github.com>
2024-02-24 09:25:15 +0530

    Add files via upload

---
+++ day 5/pro10.py
@@ -0,0 +1,13 @@
+class ece:
+    def section1(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+        print(name,rollno)
+    def section2(self,n,age,rno):
+        self.n=n
+        self.age=age
+        self.rno=rno
+        print(n,age,rno)
+obj=ece()
+obj.section1("vaishnavi","86")
+obj.section2("sri","20","01")
---
+++ day 5/pro9.py
@@ -0,0 +1,7 @@
+class student:
+    def __init__(self,name,age,sec):
+        self.name=name
+        self.age=age
+        self.sec=sec
+        print(name,age,sec)
+obj=student("vaishu","19","ece-2")
